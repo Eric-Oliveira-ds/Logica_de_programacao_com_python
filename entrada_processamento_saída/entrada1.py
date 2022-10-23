ee48f18af238282d66c7822436b94858f352741e
Eric Oliveira <eric.oliveira1717@gmail.com>
2022-10-23 12:36:20 -0300

    add first exercise in entrada processamento e saída

---
+++ entrada_processamento_saída/entrada1.py
@@ -0,0 +1,11 @@
+# Ler temperatura em graus celcius e apresenta-los convertido em graus fahrenheit
+
+print('Digite a temperatura em graus Celcius: ')
+
+C = float(input())
+
+F = (9 * C + 160)/5
+
+print('A temperatura em graus Fahrenheit é: ',F)
+
+
