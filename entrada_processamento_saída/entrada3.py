1043879dfa82e24958bd76d74bfda18533f912e7
Eric Oliveira <eric.oliveira1717@gmail.com>
2022-10-26 22:07:33 -0300

    add tree exercise for input

---
+++ entrada_processamento_saída/entrada3.py
@@ -0,0 +1,12 @@
+'''Ler a base e a altura de um triãngulo, calcular e apresentar o valor da área
+fórmula = ÁREA = (BASE * ALTURA)/2
+'''
+print('Digite o valor da base: ')
+base = float(input())
+
+print('Digite o valor da altura:')
+altura = float(input())
+
+area = (base * altura)/2
+
+print(('O valor da área é: ',area))
