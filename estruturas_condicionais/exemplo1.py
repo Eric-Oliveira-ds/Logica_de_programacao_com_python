e4a0d24d737c4e59fdfd9b0551a254043d160e52
Eric Oliveira <eric.oliveira1717@gmail.com>
2022-10-29 22:36:05 -0300

    Add exemplo1 validação de notas escolares

---
+++ estruturas_condicionais/exemplo1.py
@@ -0,0 +1,28 @@
+''' Aprovador de notas de alunos
+'''
+
+print('Digite a nota M1: ')
+m1 = float(input())
+print('Digite a nota M2: ')
+m2 = float(input())
+print('Digite a nota M3: ')
+m3 = float(input())
+
+media = (m1 + m2 + m3)/3
+
+print('Média: ', media)
+
+if media >= 6:
+    print('Aluno aprovado!')
+elif media <5:
+    print('Aluno reprovado!')
+elif media >=4 and media <6:
+    print('Aluno pegou exame!')
+    print('Digite a nota do exame: ')
+    exame = float(input())
+    if exame >= 6:
+        print('Aluno aprovado no exame')
+    else:
+        print('Aluno reprovado no exame')
+
+
